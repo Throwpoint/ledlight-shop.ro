1e404ffcc19d740ce583f413af9f99c67f48f945
Alexandru Pop <alex@seesmic.com>
2012-07-31 23:49:08 +0300

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LedlightShop::Application.config.secret_token = 'd72b443608bc35d8c7649650e385affb7168fa691a1be5610619db23611bd7149f71253eb99f8cbe9795768329106147f0c87a8cf8b8d234b0c7b6561220a2e1'
